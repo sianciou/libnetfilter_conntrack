3aeeeb36359d975a3baebacac39c690ea6c86d32
sianciou <sianciou@cht.com.tw>
2019-03-29 10:59:16 +0800

    modification for conntrack

--- src/conntrack/snprintf_default.c
+++ src/conntrack/snprintf_default.c
@@ -13,57 +13,20 @@ static int __snprintf_l3protocol(char *buf,
 				 unsigned int len,
 				 const struct nf_conntrack *ct)
 {
-	return (snprintf(buf, len, "%-8s %u ", 
+	//tcp 6
+	return (snprintf(buf, len, "%s ", 
 		l3proto2str[ct->head.orig.l3protonum] == NULL ?
-		"unknown" : l3proto2str[ct->head.orig.l3protonum], 
-		 ct->head.orig.l3protonum));
+		"unknown" : l3proto2str[ct->head.orig.l3protonum]));
 }
 
 int __snprintf_protocol(char *buf,
 			unsigned int len,
 			const struct nf_conntrack *ct)
 {
-	return (snprintf(buf, len, "%-8s %u ", 
+	//tcp 6
+	return (snprintf(buf, len, "%s ", 
 		proto2str[ct->head.orig.protonum] == NULL ?
-		"unknown" : proto2str[ct->head.orig.protonum], 
-		 ct->head.orig.protonum));
-}
-
-static int __snprintf_timeout(char *buf,
-			      unsigned int len,
-			      const struct nf_conntrack *ct)
-{
-	return snprintf(buf, len, "%u ", ct->timeout);
-}
-
-static int __snprintf_protoinfo(char *buf,
-				unsigned int len,
-				const struct nf_conntrack *ct)
-{
-	return snprintf(buf, len, "%s ",
-			ct->protoinfo.tcp.state < TCP_CONNTRACK_MAX ?
-			states[ct->protoinfo.tcp.state] :
-			states[TCP_CONNTRACK_NONE]);
-}
-
-static int __snprintf_protoinfo_sctp(char *buf,
-				     unsigned int len,
-				     const struct nf_conntrack *ct)
-{
-	return snprintf(buf, len, "%s ",
-			ct->protoinfo.sctp.state < SCTP_CONNTRACK_MAX ?
-			sctp_states[ct->protoinfo.sctp.state] :
-			sctp_states[SCTP_CONNTRACK_NONE]);
-}
-
-static int __snprintf_protoinfo_dccp(char *buf,
-				     unsigned int len,
-				     const struct nf_conntrack *ct)
-{
-	return snprintf(buf, len, "%s ",
-			ct->protoinfo.dccp.state < DCCP_CONNTRACK_MAX ?
-			sctp_states[ct->protoinfo.dccp.state] :
-			sctp_states[DCCP_CONNTRACK_NONE]);
+		"unknown" : proto2str[ct->head.orig.protonum])); 
 }
 
 static int __snprintf_address_ipv4(char *buf,
@@ -75,12 +38,10 @@ static int __snprintf_address_ipv4(char *buf,
 	int ret, size = 0, offset = 0;
 	struct in_addr src = { .s_addr = tuple->src.v4 };
 	struct in_addr dst = { .s_addr = tuple->dst.v4 };
-
-	ret = snprintf(buf, len, "%s=%s ", src_tag, inet_ntoa(src));
+	//src= dst= 
+	ret = snprintf(buf, len, "%s ", inet_ntoa(src), ntohs(tuple->l4src.tcp.port));
 	BUFFER_SIZE(ret, size, len, offset);
 
-	ret = snprintf(buf+offset, len, "%s=%s ", dst_tag, inet_ntoa(dst));
-	BUFFER_SIZE(ret, size, len, offset);
 
 	return size;
 }
@@ -101,15 +62,10 @@ static int __snprintf_address_ipv6(char *buf,
 
 	if (!inet_ntop(AF_INET6, &src, tmp, sizeof(tmp)))
 		return -1;
-
-	ret = snprintf(buf, len, "%s=%s ", src_tag, tmp);
+	//src= dst=
+	ret = snprintf(buf, len, "%s ", tmp);
 	BUFFER_SIZE(ret, size, len, offset);
 
-	if (!inet_ntop(AF_INET6, &dst, tmp, sizeof(tmp)))
-		return -1;
-
-	ret = snprintf(buf+offset, len-size, "%s=%s ", dst_tag, tmp);
-	BUFFER_SIZE(ret, size, len, offset);
 
 	return size;
 }
@@ -127,10 +83,12 @@ int __snprintf_address(char *buf,
 		size = __snprintf_address_ipv4(buf, len, tuple,
 						src_tag, dst_tag);
 		break;
-	case AF_INET6:
+
+    case AF_INET6:
 		size = __snprintf_address_ipv6(buf, len, tuple,
 						src_tag, dst_tag);
 		break;
+
 	}
 
 	return size;
@@ -148,15 +106,18 @@ int __snprintf_proto(char *buf,
 	case IPPROTO_UDPLITE:
 	case IPPROTO_SCTP:
 	case IPPROTO_DCCP:
-		return snprintf(buf, len, "sport=%u dport=%u ",
+		//sport= dport=
+		return snprintf(buf, len, "%u %u ",
 			        ntohs(tuple->l4src.tcp.port),
 			        ntohs(tuple->l4dst.tcp.port));
 		break;
-	case IPPROTO_GRE:
+
+    case IPPROTO_GRE:
 		return snprintf(buf, len, "srckey=0x%x dstkey=0x%x ",
 			        ntohs(tuple->l4src.all),
 			        ntohs(tuple->l4dst.all));
 		break;
+        
 	case IPPROTO_ICMP:
 	case IPPROTO_ICMPV6:
 		/* The ID only makes sense some ICMP messages but we want to
@@ -166,86 +127,22 @@ int __snprintf_proto(char *buf,
 			tuple->l4dst.icmp.code,
 			ntohs(tuple->l4src.icmp.id)));
 		break;
+        
 	}
 
 	return size;
 }
 
-static int
-__snprintf_tuple_zone(char *buf, unsigned int len, const char *pfx,
-		      const struct __nfct_tuple *tuple)
-{
-	return (snprintf(buf, len, "zone-%s=%u ", pfx, tuple->zone));
-}
-
-static int __snprintf_status_assured(char *buf,
-				     unsigned int len,
-				     const struct nf_conntrack *ct)
-{
-	int size = 0;
-	
-	if (ct->status & IPS_ASSURED)
-		size = snprintf(buf, len, "[ASSURED] ");
-
-	return size;
-}
-
-static int __snprintf_status_not_seen_reply(char *buf,
-					    unsigned int len,
-					    const struct nf_conntrack *ct)
-{
-	int size = 0;
-	
-        if (!(ct->status & IPS_SEEN_REPLY))
-                size = snprintf(buf, len, "[UNREPLIED] ");
-
-	return size;
-}
 
 static int __snprintf_counters(char *buf,
 			       unsigned int len,
-			       const struct nf_conntrack *ct,
-			       int dir)
+			       struct nf_conntrack *ct,
+			       int origdir, int repldir)
 {
-	return (snprintf(buf, len, "packets=%llu bytes=%llu ",
-			 (unsigned long long) ct->counters[dir].packets,
-			 (unsigned long long) ct->counters[dir].bytes));
-}
-
-static int
-__snprintf_mark(char *buf, unsigned int len, const struct nf_conntrack *ct)
-{
-	return (snprintf(buf, len, "mark=%u ", ct->mark));
-}
-
-static int
-__snprintf_secmark(char *buf, unsigned int len, const struct nf_conntrack *ct)
-{
-	return (snprintf(buf, len, "secmark=%u ", ct->secmark));
-}
-
-static int
-__snprintf_use(char *buf, unsigned int len, const struct nf_conntrack *ct)
-{
-	return (snprintf(buf, len, "use=%u ", ct->use));
-}
-
-static int
-__snprintf_id(char *buf, unsigned int len, const struct nf_conntrack *ct)
-{
-	return (snprintf(buf, len, "id=%u ", ct->id));
-}
-
-static int
-__snprintf_zone(char *buf, unsigned int len, const struct nf_conntrack *ct)
-{
-	return (snprintf(buf, len, "zone=%u ", ct->zone));
-}
-
-static int
-__snprintf_secctx(char *buf, unsigned int len, const struct nf_conntrack *ct)
-{
-	return (snprintf(buf, len, "secctx=%s ", ct->secctx));
+	//packets= bytes=
+    return (snprintf(buf, len, "%llu %llu",
+                (unsigned long long) ct->counters[origdir].bytes,
+                (unsigned long long) ct->counters[repldir].bytes));
 }
 
 static int
@@ -287,12 +184,6 @@ __snprintf_timestamp_delta(char *buf, unsigned int len,
 
 	return (snprintf(buf, len, "delta-time=%llu ",
 			(unsigned long long)delta_time));
-}
-
-static int
-__snprintf_helper_name(char *buf, unsigned int len, const struct nf_conntrack *ct)
-{
-	return (snprintf(buf, len, "helper=%s ", ct->helper_name));
 }
 
 int
@@ -352,22 +243,6 @@ int __snprintf_conntrack_default(char *buf,
 {
 	int ret = 0, size = 0, offset = 0;
 
-	switch(msg_type) {
-		case NFCT_T_NEW:
-			ret = snprintf(buf, len, "%9s ", "[NEW]");
-			break;
-		case NFCT_T_UPDATE:
-			ret = snprintf(buf, len, "%9s ", "[UPDATE]");
-			break;
-		case NFCT_T_DESTROY:
-			ret = snprintf(buf, len, "%9s ", "[DESTROY]");
-			break;
-		default:
-			break;
-	}
-
-	BUFFER_SIZE(ret, size, len, offset);
-
 	if (flags & NFCT_OF_SHOW_LAYER3) {
 		ret = __snprintf_l3protocol(buf+offset, len, ct);
 		BUFFER_SIZE(ret, size, len, offset);
@@ -375,128 +250,15 @@ int __snprintf_conntrack_default(char *buf,
 
 	ret = __snprintf_protocol(buf+offset, len, ct);
 	BUFFER_SIZE(ret, size, len, offset);
-
-	if (test_bit(ATTR_TIMEOUT, ct->head.set)) {
-		ret = __snprintf_timeout(buf+offset, len, ct);
-		BUFFER_SIZE(ret, size, len, offset);
-	}
-
-        if (test_bit(ATTR_TCP_STATE, ct->head.set)) {
-		ret = __snprintf_protoinfo(buf+offset, len, ct);
-		BUFFER_SIZE(ret, size, len, offset);
-	}
-
-	if (test_bit(ATTR_SCTP_STATE, ct->head.set)) {
-		ret = __snprintf_protoinfo_sctp(buf+offset, len, ct);
-		BUFFER_SIZE(ret, size, len, offset);
-	}
-
-	if (test_bit(ATTR_DCCP_STATE, ct->head.set)) {
-		ret = __snprintf_protoinfo_dccp(buf+offset, len, ct);
-		BUFFER_SIZE(ret, size, len, offset);
-	}
-
 	ret = __snprintf_address(buf+offset, len, &ct->head.orig,
 				 "src", "dst");
 	BUFFER_SIZE(ret, size, len, offset);
 
-	ret = __snprintf_proto(buf+offset, len, &ct->head.orig);
-	BUFFER_SIZE(ret, size, len, offset);
-
-	if (test_bit(ATTR_ORIG_ZONE, ct->head.set)) {
-		ret = __snprintf_tuple_zone(buf+offset, len, "orig", &ct->head.orig);
-		BUFFER_SIZE(ret, size, len, offset);
-	}
-
 	if (test_bit(ATTR_ORIG_COUNTER_PACKETS, ct->head.set) &&
 	    test_bit(ATTR_ORIG_COUNTER_BYTES, ct->head.set)) {
-		ret = __snprintf_counters(buf+offset, len, ct, __DIR_ORIG);
+		ret = __snprintf_counters(buf+offset, len, ct, __DIR_ORIG, __DIR_REPL);
 		BUFFER_SIZE(ret, size, len, offset);
 	}
-
-	if (test_bit(ATTR_STATUS, ct->head.set)) {
-		ret = __snprintf_status_not_seen_reply(buf+offset, len, ct);
-		BUFFER_SIZE(ret, size, len, offset);
-	}
-
-	ret = __snprintf_address(buf+offset, len, &ct->repl,
-				 "src", "dst");
-	BUFFER_SIZE(ret, size, len, offset);
-
-	ret = __snprintf_proto(buf+offset, len, &ct->repl);
-	BUFFER_SIZE(ret, size, len, offset);
-
-	if (test_bit(ATTR_REPL_ZONE, ct->head.set)) {
-		ret = __snprintf_tuple_zone(buf+offset, len, "reply", &ct->repl);
-		BUFFER_SIZE(ret, size, len, offset);
-	}
-
-	if (test_bit(ATTR_REPL_COUNTER_PACKETS, ct->head.set) &&
-	    test_bit(ATTR_REPL_COUNTER_BYTES, ct->head.set)) {
-		ret = __snprintf_counters(buf+offset, len, ct, __DIR_REPL);
-		BUFFER_SIZE(ret, size, len, offset);
-	}
-
-	if (test_bit(ATTR_STATUS, ct->head.set)) {
-		ret = __snprintf_status_assured(buf+offset, len, ct);
-		BUFFER_SIZE(ret, size, len, offset);
-	}
-
-	if (test_bit(ATTR_MARK, ct->head.set)) {
-		ret = __snprintf_mark(buf+offset, len, ct);
-		BUFFER_SIZE(ret, size, len, offset);
-	}
-
-	if (test_bit(ATTR_SECMARK, ct->head.set)) {
-		ret = __snprintf_secmark(buf+offset, len, ct);
-		BUFFER_SIZE(ret, size, len, offset);
-	}
-
-	if (test_bit(ATTR_SECCTX, ct->head.set)) {
-		ret = __snprintf_secctx(buf+offset, len, ct);
-		BUFFER_SIZE(ret, size, len, offset);
-	}
-
-	if (test_bit(ATTR_ZONE, ct->head.set)) {
-		ret = __snprintf_zone(buf+offset, len, ct);
-		BUFFER_SIZE(ret, size, len, offset);
-	}
-
-	if (test_bit(ATTR_TIMESTAMP_START, ct->head.set)) {
-		ret = __snprintf_timestamp_delta(buf+offset, len, ct);
-		BUFFER_SIZE(ret, size, len, offset);
-	}
-	if (flags & NFCT_OF_TIMESTAMP) {
-		if (test_bit(ATTR_TIMESTAMP_START, ct->head.set)) {
-			ret = __snprintf_timestamp_start(buf+offset, len, ct);
-			BUFFER_SIZE(ret, size, len, offset);
-		}
-		if (test_bit(ATTR_TIMESTAMP_STOP, ct->head.set)) {
-			ret = __snprintf_timestamp_stop(buf+offset, len, ct);
-			BUFFER_SIZE(ret, size, len, offset);
-		}
-	}
-
-	if (test_bit(ATTR_HELPER_NAME, ct->head.set)) {
-		ret = __snprintf_helper_name(buf+offset, len, ct);
-		BUFFER_SIZE(ret, size, len, offset);
-	}
-
-	if (test_bit(ATTR_USE, ct->head.set)) {
-		ret = __snprintf_use(buf+offset, len, ct);
-		BUFFER_SIZE(ret, size, len, offset);
-	}
-
-	if (flags & NFCT_OF_ID && test_bit(ATTR_ID, ct->head.set)) {
-		ret = __snprintf_id(buf+offset, len, ct);
-		BUFFER_SIZE(ret, size, len, offset);
-	}
-
-	if (map && test_bit(ATTR_CONNLABELS, ct->head.set)) {
-		ret = __snprintf_clabels(buf+offset, len, ct, map);
-		BUFFER_SIZE(ret, size, len, offset);
-	}
-
 	/* Delete the last blank space */
 	size--;
 
